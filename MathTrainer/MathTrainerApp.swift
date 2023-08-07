7c8b905b936f401b23a892d1569a7d33de71e2af
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-08-07 16:18:31 +0300

    Initial Commit

---
+++ MathTrainer/MathTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTrainerApp.swift
+//  MathTrainer
+//
+//  Created by Oleg Gavashi on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MathTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
